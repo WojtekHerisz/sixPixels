c70a1b0a959b00b77caa6e6518f5d5807a1d1db3
Kamil Błanik <kamil.blanik@ing.com>
2022-03-10 16:28:26 +0100

    resolve confl

--- src/components/Adopt/First.js
+++ src/components/Adopt/First.js
@@ -1,11 +1,14 @@
 import Search from "../Search";
 import Slider from "../Slider";
+import Map from "../../../public/assets/images/map.png";
+import Image from "next/image";
 
 const First = () => {
   return (
     <div className="">
       <Search label="" />
       <Slider />
+      <Image src={Map} alt="map" />
     </div>
   );
 };
