4b9f37816553d0fbded0848dd2321b418028d6c2
KATARZYNA.BIENKOWSKA <katarzyna.bienkowska@ing.com>
2022-03-10 19:46:55 +0100

    Box in Adpoted Bee Page

--- src/pages/adoptedBee.js
+++ src/pages/adoptedBee.js
@@ -1,13 +1,14 @@
 import ProfileHeader from "../components/ProfileHeader";
 import Image from "next/image";
+import Link from "next/link";
 
 const AdoptedBee = () => {
   return (
     <div>
       <ProfileHeader />
-      <div className="bg-white w-5/6 border-1 drop-shadow-md rounded mx-auto flex flex-col items-center">
+      <div className="bg-white w-5/6 border-1 drop-shadow-md rounded mx-auto flex flex-col">
         <p className="text-black-txt font-bold text-2xl p-4">
-          Hurray, you've adopted a&nbsp;bee!
+          Hurray, you&apos;ve adopted a&nbsp;bee!
         </p>
 
         <Image
@@ -17,15 +18,23 @@ const AdoptedBee = () => {
           height={79}
         />
 
-        <p className="text-black-txt">Radius</p>
-        <div className="flex items-center relative border rounded bg-white h-6">
+        <p className="text-black-txt mx-6 pb-2 py-3">Give your bee a name:</p>
+        <div className="flex items-center relative border rounded h-8 mx-3">
           <input
-            type="number"
-            name="radius"
-            id="number"
-            className="block text-center w-10 sm:text-sm border-placeholder-txt rounded outline-0 text-black-txt"
-            value="5"
+            type="text"
+            name="name"
+            id="name"
+            className="block text-center w-20 sm:text-sm border-placeholder-txt rounded outline-0 text-[583412] font-[Allura]"
+            defaultValue="Rosie"
           />
+        </div>
+
+        <div className="flex justify-start">
+          <Link href="/login">
+            <a className="px-6 py-1 m-4 border border-transparent text-base font-medium rounded text-white bg-moon-yellow hover:bg-[#FDCA45] md:py-4 md:text-lg md:px-10">
+              Save
+            </a>
+          </Link>
         </div>
       </div>
     </div>
