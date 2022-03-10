7645890770d85dfcea905d1f4f05e42c14d04513
Kamil Błanik <kamil.blanik@ing.com>
2022-03-10 20:06:39 +0100

    acheivments

--- src/pages/adoptedBee.js
+++ src/pages/adoptedBee.js
@@ -3,11 +3,11 @@ import Image from "next/image";
 
 const AdoptedBee = () => {
   return (
-    <div>
+    <div className="h-screen">
       <ProfileHeader />
       <div className="bg-white w-5/6 border-1 drop-shadow-md rounded mx-auto flex flex-col items-center">
         <p className="text-black-txt font-bold text-2xl p-4">
-          Hurray, you've adopted a&nbsp;bee!
+          Hurray, you&apos;ve adopted a&nbsp;bee!
         </p>
 
         <Image
@@ -26,6 +26,31 @@ const AdoptedBee = () => {
             className="block text-center w-10 sm:text-sm border-placeholder-txt rounded outline-0 text-black-txt"
             value="5"
           />
+        </div>
+      </div>
+
+      <div className="flex flex-col p-8">
+        <h2 className="font-poppins font-semibold">My achievment</h2>
+        <div className="flex items-center my-2">
+          <Image
+            src="/assets/images/award.png"
+            alt="Bee"
+            width={22}
+            height={32}
+          />
+          <span className="mx-4">Adopting your first bee</span>
+        </div>
+        <div className="h-4 w-5/6 mx-auto mt-8 rounded border border-1 border-gray-400">
+          <div className="h-4 w-1/5 rounded -translate-x-px -translate-y-px bg-bee ml-0 mr-auto" />
+        </div>
+        <span className="text-xs">
+          Adopt more bees or make donations to unlock achievments!
+        </span>
+        <h2 className="font-poppins font-semibold mt-18 mb-4">
+          My honey orders
+        </h2>
+        <div className="shadow-xl border border-1 border-gray-200 rounded text-gray-500 text-center p-4">
+          27.02.22 | Acacia | 500ml | 80 PLN
         </div>
       </div>
     </div>
